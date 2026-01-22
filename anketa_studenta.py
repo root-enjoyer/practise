bd0b01c1faf36f5f611ef1c368a407b515636e08
root-enjoyer <8v2thtxq4m@privaterelay.appleid.com>
2026-01-22 21:24:47 +0200

    Add files via upload

---
+++ anketa_studenta.py
@@ -0,0 +1,14 @@
+print("Моя анкета студента")
+print("-----------")
+
+name = "Маційчук Владислав"
+age = 16
+status = "Студент"
+group = "КІ-25"
+city = "Київ"
+
+print("Ім'я:", name)
+print("Вік:", age)
+print("Статус:", status)
+print("Група:", group)
+print("Місто проживання:", city)
